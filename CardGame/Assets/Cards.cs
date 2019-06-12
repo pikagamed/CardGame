4137164244b4395fb96e1abfacbd23cfc3e5a3db
pikagamed <51389229+pikagamed@users.noreply.github.com>
2019-06-12 16:31:27 +0800

    Declare Class and GameManager
    宣告Card型別以及遊戲管理員

---
+++ CardGame/Assets/Cards.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Cards
+{
+    #region 欄位宣告
+    private int _number;
+    private int _xPosition;
+    private Sprite _picture;
+    #endregion
+
+    #region 屬性設定
+    public int number { get { return _number; } }
+    public int xPosition { get { return _xPosition; } set { _xPosition = value; } }
+    public Sprite picture { get { return _picture; } }
+    #endregion
+
+    #region 建構方法
+    /// <summary>
+    /// Cards建構子
+    /// </summary>
+    /// <param name="number">數字</param>
+    /// <param name="xPosition">水平位置</param>
+    /// <param name="picture">圖片</param>
+    public Cards ( int number, int xPosition, Sprite picture)
+    {
+        _number = number;
+        _xPosition = xPosition;
+        _picture = picture;
+    }
+    #endregion
+}
---
+++ CardGame/Assets/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
