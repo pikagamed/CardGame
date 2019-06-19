1af94949d5d47cb009e225906c717cce8049b5c2
pikagamed <51389229+pikagamed@users.noreply.github.com>
2019-06-19 14:07:35 +0800

    CardGameFinish
    Done

--- CardGame/Assets/Cards.cs
+++ CardGame/Assets/Cards.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+[System.Serializable]
 public class Cards
 {
     #region 欄位宣告
@@ -9,6 +10,8 @@ public class Cards
     #endregion
 
     #region 屬性設定
+    //public int xPosition { get => _xPosition; set =>_xPosition = value;  }
+
     public int number { get { return _number; } }
     public int xPosition { get { return _xPosition; } set { _xPosition = value; } }
     public Sprite picture { get { return _picture; } }
@@ -26,6 +29,11 @@ public class Cards
         _number = number;
         _xPosition = xPosition;
         _picture = picture;
+
+        GameObject card = new GameObject("Card");   //Unity API 新增遊戲物件(名稱)
+        card.AddComponent<SpriteRenderer>();            //添加元件 泛型方法<類型> 
+        card.GetComponent<SpriteRenderer>().sprite = picture; //取得元件 圖片 = 屬性 - 圖片
+        card.GetComponent<Transform>().position = new Vector2(xPosition, 0);    //取得元件 變形 座標 = 新二維向量
     }
     #endregion
 }
--- CardGame/Assets/GameManager.cs
+++ CardGame/Assets/GameManager.cs
@@ -1,11 +1,69 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Cards cardPlayer, cardEnemy;
+    //public Sprite Image0, Image1, Image2, Image3, Image4,
+    //                    Image5, Image6, Image7, Image8, Image9;
+
+    public Sprite[] images;
+
+    public TextMesh resultText; //宣告文字元件
+    public TextMesh resetText; //宣告文字元件
+
     void Start()
     {
-        
+        int rPlayer, rEnemy;
+
+        rPlayer = Random.Range(0, 10);
+        rEnemy = Random.Range(0, 10);
+
+        cardPlayer = new Cards(rPlayer, -5, images[rPlayer]);
+        cardEnemy = new Cards(rEnemy, 5, images[rEnemy]);
+
+        //判斷式 IF
+        //一行判斷式  ? :
+
+        resultText.fontSize = 84;
+        if (cardPlayer.number > cardEnemy.number)
+        {
+            resultText.text = "WIN";
+            resultText.color = Color.red;
+        }
+        else if (cardPlayer.number < cardEnemy.number)
+        {
+            resultText.text = "LOSE...";
+            resultText.color = Color.blue;
+        }
+        else
+        {
+            resultText.text = "DRAW";
+            resultText.color = Color.green;
+        }
+
+        StartCoroutine(WaitSecond(2));
+        resetText.characterSize = 0.1f;
     }
 
+    //更新事件：每幀執行一次(60Hz)
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+            SceneManager.LoadScene("CardGame");
+        }
+    }
+
+    IEnumerator WaitSecond(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+    }
 }
